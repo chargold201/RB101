2d78373200e052b2498c37bb628a082f7bdca7c5
Charlotte Goldthwaite <chargold201@gmail.com>
2024-02-24 14:38:18 -0500

    Add completed triangles_2.rb

--- lesson_1/easy_1/triangles_2.rb
+++ lesson_1/easy_1/triangles_2.rb
@@ -1,11 +1,8 @@
-require 'pry'
-
 def triangle(int)
   i = int
   while i > 0
     spaces = ' ' * (i - 1)
     stars = '*' * (int - spaces.length)
-    # binding.pry
     puts (spaces + stars)
     i = i - 1
   end
